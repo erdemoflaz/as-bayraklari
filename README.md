7fcb657fad4147b67262360f8e8c251ea639f88a
Erdem Oflaz <erdemoflaz4@gmail.com>
2018-04-17 16:54:09 +0300

    ✨  asss bayraklarıııı

---
+++ README.md
@@ -0,0 +1,17 @@
+# as-bayraklari
+
+An initiative, using frames from [`as-bayraklari`](https://github.com/erdemoflaz/as-bayraklari), to allow any computer with `curl` to display an _animated_ turkish flag.
+
+# Try it!
+```bash
+node index.js
+curl localhost:3000
+```
+
+# As bayrakları as as!
+<div align="center">
+  <img src='/turkish-flag.gif' />
+</div>
+
+# Creators
+* [Erdem OFLAZ](https://github.com/erdemoflaz/) & [Barış ESEN](https://github.com/barisesen)
---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('mz/fs');
+const http = require('http');
+const {Readable} = require('stream');
+const colors = require('colors/safe');
+const frames = [];
+
+fs.readdir('./frames').then(data => {
+  data.forEach(async frame => {
+    const f = await fs.readFile(`./frames/${frame}`);
+    frames.push(f.toString());
+  })
+});
+
+const streamer = stream => {
+  let index = 0;
+  return setInterval(() => {
+    if (index >= frames.length) index = 0; stream.push('\033[2J\033[H');
+      stream.push(colors['red'](frames[index]));
+      index++;
+  }, 70);
+}
+
+const server = http.createServer((req, res) => {
+  const stream = new Readable();
+  stream._read = function noop () {};
+  stream.pipe(res);
+  const interval = streamer(stream);
+  req.on('close', () => {
+    stream.destroy();
+    clearInterval(interval);
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, err => {
+  if (err) throw err;
+  console.log(`Listening on locahost:${port}`);
+});
